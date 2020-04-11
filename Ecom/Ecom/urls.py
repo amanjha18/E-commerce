50b87411e20a248c416554778e3db7aa8b7216f1
amanjha <amanjha813020@gmail.com>
2020-04-12 04:56:51 +0530

    add home page

--- Ecom/Ecom/urls.py
+++ Ecom/Ecom/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
+    path('', include ('shop.urls'))
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
